3a1cf13ce4e6f26586873441cc73d2dd232148e4
AnnaKhmialnitskaya <anna.khmialnitskaya@gmail.com>
2017-03-14 21:39:56 +0300

    Add test for method createCityMap

--- src/test/java/by/htp/newGame/NewGameServiceTest.java
+++ src/test/java/by/htp/newGame/NewGameServiceTest.java
@@ -3,9 +3,11 @@ package by.htp.newGame;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.Before;
@@ -63,5 +65,33 @@ public class NewGameServiceTest {
 		assertEquals(new Character('И'), service.getLastChar("Алтай"));
 		assertEquals(new Character('М'), service.getLastChar("Римь"));
 		assertEquals(new Character('М'), service.getLastChar("Римъ"));
+	}
+
+	@Test
+	public void createCityMap() {
+		// create data
+		Set<String> citiesSet = new HashSet<>();
+		citiesSet.add("Новосибирск");
+		citiesSet.add("Новороссийск");
+		citiesSet.add("Ухта");
+		// check cityMap
+		Map<Character, Set<String>> cityMap = service.createCityMap(citiesSet);
+		assertNotNull(cityMap);
+		// check keySet
+		Set<Character> keySet = cityMap.keySet();
+		assertEquals(2, keySet.size());
+		assertTrue(keySet.contains(new Character('Н')));
+		assertTrue(keySet.contains(new Character('У')));
+		// check cities with letter 'Н'
+		Set<String> setLetterN = cityMap.get(new Character('Н'));
+		assertNotNull(setLetterN);
+		assertEquals(2, setLetterN.size());
+		assertTrue(setLetterN.contains("Новосибирск"));
+		assertTrue(setLetterN.contains("Новороссийск"));
+		// check cities with letter 'У'
+		Set<String> setLetterY = cityMap.get(new Character('У'));
+		assertNotNull(setLetterY);
+		assertEquals(1, setLetterY.size());
+		assertTrue(setLetterY.contains("Ухта"));
 	}
 }
--- src/test/java/by/htp/newGame/NewGameServiceTestNG.java
+++ src/test/java/by/htp/newGame/NewGameServiceTestNG.java
@@ -3,9 +3,11 @@ package by.htp.newGame;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.testng.annotations.BeforeClass;
@@ -62,5 +64,33 @@ public class NewGameServiceTestNG {
 		assertEquals(new Character('И'), service.getLastChar("Алтай"));
 		assertEquals(new Character('М'), service.getLastChar("Римь"));
 		assertEquals(new Character('М'), service.getLastChar("Римъ"));
+	}
+
+	@Test
+	public void createCityMap() {
+		// create data
+		Set<String> citiesSet = new HashSet<>();
+		citiesSet.add("Новосибирск");
+		citiesSet.add("Новороссийск");
+		citiesSet.add("Ухта");
+		// check cityMap
+		Map<Character, Set<String>> cityMap = service.createCityMap(citiesSet);
+		assertNotNull(cityMap);
+		// check keySet
+		Set<Character> keySet = cityMap.keySet();
+		assertEquals(2, keySet.size());
+		assertTrue(keySet.contains(new Character('Н')));
+		assertTrue(keySet.contains(new Character('У')));
+		// check cities with letter 'Н'
+		Set<String> setLetterN = cityMap.get(new Character('Н'));
+		assertNotNull(setLetterN);
+		assertEquals(2, setLetterN.size());
+		assertTrue(setLetterN.contains("Новосибирск"));
+		assertTrue(setLetterN.contains("Новороссийск"));
+		// check cities with letter 'У'
+		Set<String> setLetterY = cityMap.get(new Character('У'));
+		assertNotNull(setLetterY);
+		assertEquals(1, setLetterY.size());
+		assertTrue(setLetterY.contains("Ухта"));
 	}
 }
